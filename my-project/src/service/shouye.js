e7a899ea1299d894b8fc7ef933f6eb848d715d65
Yufeng <17521064533@163.com>
2019-08-15 20:54:26 +0800

    bug

--- my-project/src/service/shouye.js
+++ my-project/src/service/shouye.js
@@ -15,7 +15,7 @@ export function shouyehaowu() {
     return fly.post("/api/open/page/home/list/category")
 }
 
-//首页数据2
-export function shouyelist() {
-  return fly.post("/api/open/page/home/products/1.0.0")
-}
+// //首页数据2
+// export function shouyelist() {
+//   return fly.post("/api/open/page/home/products/1.0.0")
+// }
